99a4aee008b56fe7263c531cdf0262ab14bd594b
MiaLuna23 <166016904+MiaLuna23@users.noreply.github.com>
2024-04-04 08:28:20 -0300

    agrgar archivo de iniciales

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("la suma es"+ (2+2))
+console.log("la resta es"+ (6-4))
+console.log("la multiplicacion es"+ (5*4))
+console.log("la division es"+ (12/2))
+//creamos variables para la operacion
+let numero=4
+let numero=2
+let numero=20
+let numero=6
+//mostrar valor de variable
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+console.log(numero4)
+//solicitar la info al usuario
+let añoActual=prompt("ingrsa el año actual")
+let añoNacimiento=prompt ("ingresa tu año de nacimiento")
+alert("la edad es:"+ (2024-2009))
+let nombre= prompt("ingresa tu nombre")
+alert("bienvenido" + nombre)
+let nombreAlumno= "Renata"
+let apellidoAlumno= "Lotto"
+console.log("bienvenido"+ nombreAlumno+apellidoAlumno)
+alert("bienvenido"+ nombreAlumno+apellidoAlumno)
+let diez=10
+let tres=3
+let resultradoMultiplicacion= 10*3
+alert("el resultadode la multiplicacion es"+ (10*3))
